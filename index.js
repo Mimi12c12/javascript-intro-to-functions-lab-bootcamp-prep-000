b336c2ac448436445d740cb81a07a3bc129a25ac
Misha Chapman <mishachapman14@hotmail.com>
2019-04-29 15:11:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -24,10 +24,10 @@ function sayHiToGrandma(string){
 function sayHiToGrandma(string){
   var uppercase = "HELLO"
   var lowercase = "hello"
-
-  if (string === lowercase){
   if (string === string.toLowerCase()){
-    return "I can't hear you!"
+    return "I can't hear you!"}
+    
+    
   }else if (string === uppercase){
   }else if (string === string.toUpperCase()){
     return "YES INDEED!"
