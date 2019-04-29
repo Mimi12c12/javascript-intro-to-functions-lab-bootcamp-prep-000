c81943a06f0b9b3ff788100265035eb902cbeec0
Misha Chapman <mishachapman14@hotmail.com>
2019-04-29 15:08:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,10 +10,4 @@ function logShout(string){
 function logWhisper(string){
   console.log(string.toLowerCase())
 }
-var uppercase = "HELLO!"
-uppercase.toUpperCase()===uppercase
-var lowercase = "hello!"
-lowercase.toLowerCase()===lowercase
-var mixedCase = "Hi there!"
-mixedCase.toLowerCase() === mixedCase
-mixedCase.toUpperCase() === mixedCase
+
